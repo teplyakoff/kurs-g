0ae420bd0eb6b2a1d960567808d0200de165c604
Dmitry Teplyakov <dmitry.teplyakov@together.com>
2014-10-01 18:08:19 +0300

    Add minfin.com.ua source

--- popup.js
+++ popup.js
@@ -5,6 +5,14 @@ function kursComUaInformerInterbank() {
     container.parentNode.innerHTML = iframe;
 }
 
+function kursMinfinInterbank() {
+    var rand_no = Math.random();
+    var pic = Math.round(rand_no * 10000000);
+    var informer_usd = document.getElementById('minfincomua_i_usd');
+    informer_usd.innerHTML = '<a href="http://minfin.com.ua/currency/"><img src="http://informer.minfin.com.ua/interbank/img/usd.png?' + pic + '" width="220" height="175" border="0" alt="interbank for minfin.com.ua"></a>';
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     kursComUaInformerInterbank();
+    kursMinfinInterbank();
 });
