43c23194bdceb85e4a0c6dd2c125f710ddc508af
Dmitry Teplyakov <dmitry.teplyakoff@gmail.com>
2014-10-01 22:29:35 +0300

    Fix width of kurs informer

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 function kursComUaInformerInterbank() {
     var rnd = +new Date;
-    var iframe = '<ifr' + 'ame src="http://kurs.com.ua/informer/mezhbank/usd?color=green&rnd=' + rnd + '" width="300" height="203" frameborder="0" vspace="0" scrolling="no" hspace="0"></ifr' + 'ame>';
-    var container = document.getElementById('kurs-com-ua-informer-mezhbank-300x203-usd-green');
+    var iframe = '<ifr' + 'ame src="http://kurs.com.ua/informer/mezhbank/usd?color=green&rnd=' + rnd + '" width="220" height="203" frameborder="0" vspace="0" scrolling="no" hspace="0"></ifr' + 'ame>';
+    var container = document.getElementById('kurs-com-ua-informer-mezhbank-220x203-usd-green');
     container.parentNode.innerHTML = iframe;
 }
 
