0d49666b3831969860435b727fa0631bce4f9504
Dmitry Teplyakov <dmitry.teplyakoff@gmail.com>
2014-10-05 22:29:04 +0300

    Minfin source to use object width/height

--- js/modules/source/minfin.js
+++ js/modules/source/minfin.js
@@ -8,7 +8,7 @@ define({
     var rand_no = Math.random();
     var pic = Math.round(rand_no * 10000000);
     var informer_usd = document.getElementById('minfincomua_i_usd');
-    informer_usd.innerHTML = '<a href="http://minfin.com.ua/currency/"><img src="http://informer.minfin.com.ua/interbank/img/usd.png?' + pic + '" width="220" height="175" border="0" alt="interbank for minfin.com.ua"></a>';
+    informer_usd.innerHTML = '<a href="http://minfin.com.ua/currency/"><img src="http://informer.minfin.com.ua/interbank/img/usd.png?' + pic + '" width="' + this.width + '" height="' + this.height + '" border="0" alt="interbank for minfin.com.ua"></a>';
     if (onLoadCallback) {
       onLoadCallback();
     }
