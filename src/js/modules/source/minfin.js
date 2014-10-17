d410603200e030566c6feb643269effff64a7581
Dmitry Teplyakov <dmitry.teplyakoff@gmail.com>
2014-10-18 00:07:47 +0300

    Move minfin html to template

--- src/js/modules/source/minfin.js
+++ src/js/modules/source/minfin.js
@@ -1,26 +1,22 @@
-define({
-  width: 220,
-  height: 175,
-  setCurrency: function(currency) {
-    this.currency = currency;
-  },
-  getHtml: function () {
-    return '<div id="minfincomua_i_' + this.currency + '" style="text-align: center">Loading...</div>';
-  },
-  run: function (onLoadCallback) {
-    var rand_no = Math.random();
-    var pic = Math.round(rand_no * 10000000);
-    var informer_usd = document.getElementById('minfincomua_i_' + this.currency);
-
-    informer_usd.innerHTML = '<a target="_blank" href="http://minfin.com.ua/currency/mb/' + this.currency + '">'
-      + '<img src="http://informer.minfin.com.ua/interbank/img/' + this.currency
-      + '.png?' + pic
-      + '" width="' + this.width
-      + '" height="' + this.height
-      + '" border="0" alt="interbank for minfin.com.ua"></a>';
-
-    if (onLoadCallback) {
-      onLoadCallback(this);
+define(['underscore', 'text!source/minfin/template.html'], function (_, template) {
+  return {
+    width: 220,
+    height: 175,
+    setCurrency: function(currency) {
+      this.currency = currency;
+    },
+    getHtml: function () {
+      return _.template(template)({
+        currency: this.currency,
+        pic: Math.round(Math.random() * 10000000),
+        width: this.width,
+        height: this.height
+      });
+    },
+    run: function (onLoadCallback) {
+      if (onLoadCallback) {
+        onLoadCallback(this);
+      }
     }
   }
 });
---
+++ src/js/modules/source/minfin/template.html
@@ -0,0 +1,9 @@
+<a target="_blank" href="http://minfin.com.ua/currency/mb/<%= currency %>">
+  <img
+    src="http://informer.minfin.com.ua/interbank/img/<%= currency %>.png?<%= pic %>"
+    width="<%= width %>"
+    height="<%= height %>"
+    border="0"
+    alt="interbank for minfin.com.ua"
+  >
+</a>
